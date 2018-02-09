01b74cb0cdaa601775afa4cf6b70b70d003b6696
Scott Rigby <scott@r6by.com>
2018-02-08 22:59:57 -0500

    Push source to GCP storage so cloudbuild has something to work with

--- main.go
+++ main.go
@@ -4,19 +4,25 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 
+	"cloud.google.com/go/storage"
 	"github.com/ghodss/yaml"
 	"golang.org/x/oauth2/google"
 	cloudbuild "google.golang.org/api/cloudbuild/v1"
 	"google.golang.org/api/googleapi"
 )
 
+const (
+	cloudbuildYAML  = "source/cloudbuild.yaml"
+	storageFileName = "source.tgz"
+)
+
 func main() {
-	file := "source/cloudbuild.yaml"
-	y, err := ioutil.ReadFile(file)
+	y, err := ioutil.ReadFile(cloudbuildYAML)
 	if err != nil {
 		fmt.Print(err)
 	}
@@ -30,7 +36,25 @@ func main() {
 	fmt.Println(string(j))
 
 	projectID := os.Getenv("PROJECT_ID")
-	build := &cloudbuild.Build{}
+	storageBucketName := projectID + "_cloudbuild"
+	storageBucket, err := getStorageBucket(storageBucketName)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+	url, err := copySourceToStorage(storageBucket, storageBucketName)
+	if err != nil {
+		fmt.Printf("could not copy source to storage: %v\n", err)
+		return
+	}
+	fmt.Printf("source url: %s\n", url)
+
+	build := &cloudbuild.Build{
+		Substitutions: map[string]string{"_PROJECT_ID": projectID},
+		Source: &cloudbuild.Source{
+			StorageSource: &cloudbuild.StorageSource{
+				Bucket: storageBucketName,
+				Object: "source.tgz"}}}
 	err2 := json.Unmarshal([]byte(j), build)
 	if err2 != nil {
 		fmt.Print(err2)
@@ -40,7 +64,7 @@ func main() {
 	// > User-defined substitutions must begin with an underscore (_) and use
 	// 	 only uppercase-letters and numbers (respecting the regular expression
 	//	 _[A-Z0-9_]+).
-	build.Substitutions = map[string]string{"_PROJECT_ID": projectID}
+	// build.Substitutions = map[string]string{"_PROJECT_ID": projectID}
 	fmt.Printf("build: %+v\n", build)
 
 	operation, err := TriggerCloudBuild(projectID, build)
@@ -57,6 +81,41 @@ func readFile(file string) []byte {
 		fmt.Print(err)
 	}
 	return b
+}
+
+func getStorageBucket(name string) (*storage.BucketHandle, error) {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return client.Bucket(name), nil
+}
+
+// Thanks to GoogleCloudPlatform/golang-samples.
+// ref: https://github.com/GoogleCloudPlatform/golang-samples/blob/master/getting-started/bookshelf/app/app.go#L218
+func copySourceToStorage(storageBucket *storage.BucketHandle, storageBucketName string) (url string, err error) {
+	ctx := context.Background()
+	w := storageBucket.Object(storageFileName).NewWriter(ctx)
+	w.ACL = []storage.ACLRule{{Entity: storage.AllUsers, Role: storage.RoleReader}}
+	w.ContentType = "application/gzip"
+	w.CacheControl = "public, max-age=86400"
+
+	in, err := os.Open(storageFileName)
+	if err != nil {
+		return
+	}
+	defer in.Close()
+
+	if _, err := io.Copy(w, in); err != nil {
+		return "", err
+	}
+	if err := w.Close(); err != nil {
+		return "", err
+	}
+
+	const publicURL = "https://storage.googleapis.com/%s/%s"
+	return fmt.Sprintf(publicURL, storageBucketName, storageFileName), nil
 }
 
 // TriggerCloudBuild triggers a GCP Cloudbuild.
